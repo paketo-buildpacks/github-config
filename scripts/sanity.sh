3729a317de5d7f4298b79febef978eeeadeb773e
Arjun Sreedharan <asreedharan@vmware.com>
2020-05-15 14:51:37 +0000

    Wire repo check to run on PR
    Signed-off-by: Arjun Sreedharan <asreedharan@vmware.com>

--- scripts/sanity.sh
+++ scripts/sanity.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 # Checks if the contents of implementation/ and language-family/ follow rules
-# Future TODO: Run this on every PR
 set -euo pipefail
 
 ROOTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
@@ -13,6 +12,18 @@ check_sanity () {
             echo All files in "$cnbdir/" must be directories. Exiting.
             exit 1
         fi
+    done
+
+    # Rule 2 - Data files must be single line records of repo names
+    for datafile in 'implementation-cnbs' 'language-family-cnbs' ; do
+        lnum=1
+        while read line; do
+            if [[ ! "$line" =~ paketo-buildpacks/[A-Za-z0-9_.-] ]]; then
+                echo "Invalid data file "$datafile". (line: $lnum)"
+                exit 1
+            fi
+            lnum=$((lnum+1))
+        done < "${ROOTDIR}/.github/data/$datafile"
     done
 }
 
