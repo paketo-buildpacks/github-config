cb8fe33e63b53fa2786a5db047d3bc7321693275
Daniel Thornton <dthornton@vmware.com>
2020-07-08 10:13:50 -0400

    add support for updating package.toml files
    Signed-off-by: Ryan Moran <rmoran@vmware.com>

--- actions/dependency/update/entrypoint/main.go
+++ actions/dependency/update/entrypoint/main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/BurntSushi/toml"
 )
@@ -43,6 +44,21 @@ type Buildpack struct {
 	} `toml:"order"`
 }
 
+type PackageDependency struct {
+	Image string `toml:"image"`
+}
+
+type PackageFile struct {
+	Buildpack struct {
+		URI string `toml:"uri"`
+	} `toml:"buildpack"`
+	Dependencies []PackageDependency `toml:"dependencies"`
+}
+
+func (p PackageFile) Empty() bool {
+	return p.Buildpack.URI == "" && len(p.Dependencies) == 0
+}
+
 func main() {
 	var workspacePath string
 	flag.StringVar(&workspacePath, "workspace-path", "/github/workspace", "path to the workspace")
@@ -54,20 +70,28 @@ func main() {
 	}
 
 	fmt.Println("Updating buildpack.toml")
-
 	buildpack, err := ParseBuildpack(filepath.Join(workspacePath, "buildpack.toml"))
+	if err != nil {
+		fail(err)
+	}
+	packageFile, err := ParsePackageFile(filepath.Join(workspacePath, "package.toml"))
 	if err != nil {
 		fail(err)
 	}
 
 	switch event.ClientPayload.Strategy {
 	case "replace":
-		buildpack = Replace(buildpack, event.ClientPayload.Dependency)
+		buildpack, packageFile = Replace(buildpack, packageFile, event.ClientPayload.Dependency)
 	default:
 		fail(fmt.Errorf("unknown update strategy %q", event.ClientPayload.Strategy))
 	}
 
 	err = RenderBuildpack(buildpack, filepath.Join(workspacePath, "buildpack.toml"))
+	if err != nil {
+		fail(err)
+	}
+
+	err = RenderPackageFile(packageFile, filepath.Join(workspacePath, "package.toml"))
 	if err != nil {
 		fail(err)
 	}
@@ -91,6 +115,26 @@ func ParseEvent(path string) (Event, error) {
 	fmt.Printf("  Strategy:   %s\n", event.ClientPayload.Strategy)
 
 	return event, nil
+}
+
+func ParsePackageFile(path string) (PackageFile, error) {
+	file, err := os.Open(path)
+	switch {
+	case os.IsNotExist(err):
+		return PackageFile{}, nil
+	case err != nil:
+		return PackageFile{}, fmt.Errorf("Error: failed to read package.toml: %w", err)
+	}
+	defer file.Close()
+
+	var packageFile PackageFile
+	_, err = toml.DecodeReader(file, &packageFile)
+	if err != nil {
+		return PackageFile{}, fmt.Errorf("Error: failed to decode package.toml: %w", err)
+	}
+
+	return packageFile, nil
+
 }
 
 func ParseBuildpack(path string) (Buildpack, error) {
@@ -124,17 +168,56 @@ func RenderBuildpack(buildpack Buildpack, path string) error {
 	return nil
 }
 
-func Replace(buildpack Buildpack, dependency Dependency) Buildpack {
-	var replaced bool
-	for index, dep := range buildpack.Metadata.Dependencies {
-		if dep.ID == dependency.ID {
-			buildpack.Metadata.Dependencies[index] = dependency
-			replaced = true
+func RenderPackageFile(packageFile PackageFile, path string) error {
+	file, err := os.OpenFile(path, os.O_TRUNC|os.O_WRONLY, 0644)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return nil
 		}
+
+		return fmt.Errorf("failed to write package.toml: %w", err)
+	}
+	defer file.Close()
+
+	fmt.Println("Updating package.toml")
+
+	err = toml.NewEncoder(file).Encode(packageFile)
+	if err != nil {
+		return fmt.Errorf("failed to write package.toml: %w", err)
 	}
 
-	if !replaced {
-		buildpack.Metadata.Dependencies = append(buildpack.Metadata.Dependencies, dependency)
+	return nil
+}
+
+func Replace(buildpack Buildpack, packageFile PackageFile, dependency Dependency) (Buildpack, PackageFile) {
+	var replaced bool
+	if packageFile.Empty() {
+		for index, dep := range buildpack.Metadata.Dependencies {
+			if dep.ID == dependency.ID {
+				buildpack.Metadata.Dependencies[index] = dependency
+				replaced = true
+			}
+		}
+
+		if !replaced {
+			buildpack.Metadata.Dependencies = append(buildpack.Metadata.Dependencies, dependency)
+		}
+	} else {
+		for index, dep := range packageFile.Dependencies {
+			stripRegPrefix := strings.TrimPrefix(dep.Image, "gcr.io/")
+			if strings.HasPrefix(stripRegPrefix, dependency.ID) {
+				packageFile.Dependencies[index].Image = "gcr.io/" + dependency.ID + ":" + dependency.Version
+				replaced = true
+			}
+		}
+		if !replaced {
+			packageFile.Dependencies = append(
+				packageFile.Dependencies,
+				PackageDependency{
+					Image: "gcr.io/" + dependency.ID + ":" + dependency.Version,
+				},
+			)
+		}
 	}
 
 	for i, order := range buildpack.Order {
@@ -145,7 +228,7 @@ func Replace(buildpack Buildpack, dependency Dependency) Buildpack {
 		}
 	}
 
-	return buildpack
+	return buildpack, packageFile
 }
 
 func fail(err error) {
