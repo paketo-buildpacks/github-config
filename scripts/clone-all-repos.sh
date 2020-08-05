551111128d8b15732245d08024f5d4b19b81812b
Ryan Moran <rmoran@vmware.com>
2020-08-05 17:08:17 -0400

    Cleanup

--- scripts/clone-all-repos.sh
+++ scripts/clone-all-repos.sh
@@ -1,56 +1,48 @@
 #!/usr/bin/env bash
 
-set -e
-set -u
+set -eu
 set -o pipefail
 
 readonly ROOTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 
-extra_repos=$(cat <<-END
-paketo-buildpacks/packit
-paketo-buildpacks/occam
-paketo-buildpacks/github-config
-paketo-buildpacks/paketo-website
-END
-)
+function main() {
+  local repos line
+  repos="$(
+    cat "${ROOTDIR}/.github/data/implementation-cnbs"
+    cat "${ROOTDIR}/.github/data/language-family-cnbs"
+    cat <<-EOF
+      paketo-buildpacks/packit
+      paketo-buildpacks/occam
+      paketo-buildpacks/github-config
+      paketo-buildpacks/paketo-website
+		EOF
+  )"
 
-function clone_all_repos() {
-  local line
-  pushd "${HOME}/workspace" > /dev/null
-    for datafile in 'implementation-cnbs' 'language-family-cnbs' ; do
-      while read -r line; do
-       clone_or_pull "${line}"
-      done < "${ROOTDIR}/.github/data/${datafile}"
-    done
-
-    for line in ${extra_repos};
-    do
-       clone_or_pull "${line}"
-    done <<< "${extra_repos}"
-  popd > /dev/null
+  while read -r line; do
+    clone_or_pull "${line}"
+  done < <( echo "${repos}" | sort)
 }
 
 function clone_or_pull() {
-  local repo name org
+  local repo
   repo="${1}"
-  org=$(echo "${repo}" | cut -d '/' -f1)
-  name=$(echo "${repo}" | cut -d '/' -f2)
 
-  mkdir -p "${org}"
-  pushd "${org}" > /dev/null
-    if [[ ! -d "${name}" ]]; then
-      echo "Cloning ${repo}"
-      git clone "git@github.com:${repo}.git"
-    else
-      echo "${repo} already cloned, updating"
-      pushd "${name}" > /dev/null
-        git co master ## Change to main when we switch all of our repos over
-        git pull
-      popd > /dev/null
-    fi
-    echo ""
-  popd > /dev/null
+  local path
+  path="${HOME}/workspace/${repo}"
+
+  mkdir -p "$(dirname "${path}")"
+
+  if [[ ! -d "${path}" ]]; then
+    echo "Cloning ${repo}"
+    git clone "git@github.com:${repo}.git" "${path}"
+  else
+    echo "${repo} already cloned, updating"
+
+    git -C "${path}" checkout main
+    git -C "${path}" pull --rebase
+  fi
+
+  echo
 }
 
-
-clone_all_repos "${@:-}"
+main "${@:-}"
