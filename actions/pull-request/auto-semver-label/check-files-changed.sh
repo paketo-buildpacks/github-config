bba8cd4fb7eb1e035c5442299faaee35d787b388
Frankie G-J <frankieg@vmware.com>
2021-10-27 14:07:37 -0400

    Enforce and auto-add semver PR labels (#360)
    * add PR semver label action
    
    * polish action
    
    * add autolabel workflows
    
    and .patch_files for allowlist of patch changes
    
    * extract file-checking into a script

---
+++ actions/pull-request/auto-semver-label/check-files-changed.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+set -eu
+set -o pipefail
+
+function main() {
+  local repo number patchfiles
+
+  while [ "${#}" != 0 ]; do
+    case "${1}" in
+      --repo)
+        repo="${2}"
+        shift 2
+        ;;
+
+      --number)
+        number="${2}"
+        shift 2
+        ;;
+
+      --patchfiles)
+        patchfiles="${2}"
+        shift 2
+        ;;
+
+      "")
+        shift
+        ;;
+
+      *)
+        echo "unknown argument \"${1}\""
+        exit 1
+    esac
+  done
+
+  while read -r changed; do
+    echo "File changed: ${changed}"
+    # scan through an allow list. does each element of the changed files match something in the allow list?
+    safe=0
+    while read -r file; do
+      if [[ "${changed}" =~ ${file} ]]; then
+        echo "${changed} is on the patch allowlist"
+        safe=1
+        break
+      fi
+    done < "${patchfiles}"
+
+    if [ "${safe}" -eq "0" ]; then
+      echo "Files changed that aren't on the patch allowlist"
+      echo "::set-output name=label::"
+      exit 0
+    fi
+  done < <(gh api /repos/"${repo}"/pulls/"${number}"/files --jq '.[].filename')
+
+  echo "All changes are patches."
+  echo "::set-output name=label::patch"
+}
+
+main "${@:-}"
