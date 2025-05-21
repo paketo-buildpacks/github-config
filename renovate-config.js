facfe60a0aab5a63b4955921181490ab06ff105b
Nicolas Bender <nicolas.bender@sap.com>
2025-05-21 18:07:24 +0200

    Add renovate

---
+++ renovate-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    username: 'renovate-release',
+    gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
+    onboarding: false,
+    platform: 'github',
+    forkProcessing: 'enabled',
+    repositories: ['paketo-buildpacks/occam']
+}
